42cbc6c4c1d5583c38801332b7e73ecb1c2ce29d
chen liang <chenliang@whyhowinfo.com>
2020-07-02 19:01:12 +0800

    add faster rcnn files

---
+++ detecation_models_pytorch/faster_rcnn/model.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class FasterRCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.cov2_1= nn.Conv2d()
+        pass
+    def forward(self,x):
+        pass
